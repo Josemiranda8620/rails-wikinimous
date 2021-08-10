bac4db9ad29f8a795af044feef8a6e024d8458eb
Jose Miranda <josemiranda8620@hotmail.com>
2021-08-10 16:55:59 +0900

    wikinimous challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+article1 = Article.new(title: 'Test1', content: "Content for test1")
+article2 = Article.new(title: 'Test2', content: "Content for test2")
+article3 = Article.new(title: 'Test3', content: "Content for test3")
+article4 = Article.new(title: 'Test4', content: "Content for test4")
+article5 = Article.new(title: 'Test5', content: "Content for test5")
+
+articles = [article1, article2, article3, article4, article5]
+
+articles.each do |article|
+    article.save
+end    
+
